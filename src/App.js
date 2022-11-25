0cf0934580dfe2e1e2ec8ddf5e48722e2f7dd2bc
jlum67 <90434758+jlum67@users.noreply.github.com>
2022-11-25 18:33:29 -0500

    prg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import data from "./assets/data.json";
+import { ReactDOM } from 'react-dom';
+import { RadioGroup,FormControl,FormLabel, FormControlLabel, Radio } from '@mui/material';
+
+import Politician from "./components/Politician";
+
+/* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
+data.forEach((item) => {
+  item.image = process.env.PUBLIC_URL + "/" + item.image;
+});
+/* ############################################################## */
 
 function App() {
+  // TODO: use useState to create a state variable to hold the state of the cart
+  /* add your cart state code here */
+
+  // const [total, setTotal] = useState(0)
+  const [cart, setCart] = useState([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>House of Representatives Vote Tracker</h1> {/* TODO: personalize your bakery (if you want) */}
+
+
+      <div>
+
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Party</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="all"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel value="democrat" control={<Radio />} label="Democrat" />
+            <FormControlLabel value="republican" control={<Radio />} label="Republican" />
+            <FormControlLabel value="all" control={<Radio />} label="All" />
+
+          </RadioGroup>
+        </FormControl>
+
+      </div>
+
+      <br></br>
+      <br></br>
+
+      <div>
+
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Legislative Body</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="all"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel value="house" control={<Radio />} label="House" />
+            <FormControlLabel value="senate" control={<Radio />} label="Senate" />
+            <FormControlLabel value="all" control={<Radio />} label="All" />
+
+          </RadioGroup>
+        </FormControl>
+
+
+      </div>
+
+
+
+      <div>
+        {data.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+          // <p>Bakery Item {index}</p> // replace with BakeryItem componen
+          <Politician name={item.name} title={item.title} leg={item.legislativebody} age={item.age} party={item.party} image={item.image} cart={cart} setCart={setCart} />
+        ))}
+      </div>
+
+
+
+      <div>
+        <h2>Supporters</h2>
+        <h3>
+
+          {cart.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+          // <p>Bakery Item {index}</p> // replace with BakeryItem component
+          <div>
+            {item}
+          </div>
+          ))}
+        
+        </h3>
+        {/* <h3>Total: {total}</h3> */}
+  
+        {/* TODO: render a list of items in the cart */}
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Politician.js
@@ -0,0 +1,44 @@
+import "./politician.css";
+import { useState } from "react";
+
+export default function Politician({name, title, leg, age, party, image, cart, setCart}) {
+
+    // const name = props.name;
+    // const description = props.description;
+    // const price = props.price;
+    // const image = props.image;
+
+
+	return (
+
+        <div class="border">
+            <h4>
+                <img src={image} width="300rem" height="330rem"/>
+            </h4>
+            <h4>
+                {name}
+            </h4>
+            <h5>
+                Title: {title}
+                <br/>
+                Legislative Body: {leg}
+                <br/>
+                Age: {age}
+                <br/>
+                Party: {party}
+            </h5>
+
+            <button onClick={() => {
+                    // setTotal(total + price) 
+                    setCart([...cart, name])
+                }
+            }>In Support</button>
+
+        </div>
+
+
+	);
+}
+
+
+
