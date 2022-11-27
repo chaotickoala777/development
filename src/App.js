90898fdb81957371ec40255b080112969b017278
jlum67 <90434758+jlum67@users.noreply.github.com>
2022-11-27 16:37:04 -0500

    prog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import data from "./assets/data.json";
 import { ReactDOM } from 'react-dom';
 import { RadioGroup,FormControl,FormLabel, FormControlLabel, Radio } from '@mui/material';
@@ -19,10 +19,26 @@ function App() {
 
   // const [total, setTotal] = useState(0)
   const [cart, setCart] = useState([])
-  const [value, setValue] = useState("all");
+  // const [value, setValue] = useState("all");
+  const [filter, setFilter] = useState("all");
 
-  const handleChange = event => {
-    setValue(event.target.value)
+  // useEffect(() => {
+  //   setFilter(data);
+  // }, []);
+
+  function filtering(ptype) {
+    // let filter = data.filter(type => type.party === ptype);
+    console.log(data)
+    let filter = data.filter(item => item.party === ptype)
+    console.log(filter)
+    return filter;
+  }
+
+  function handleType(e) {
+    let typeFilter = e.target.value;
+    typeFilter !== "all"
+      ? setFilter(filtering(typeFilter))
+      : setFilter(data);
   }
 
   return (
@@ -39,12 +55,11 @@ function App() {
               aria-labelledby="demo-radio-buttons-group-label"
               defaultValue="all"
               name="radio-buttons-group"
-              value={value}
-              onChange={handleChange}
+              onChange={handleType}
             >
-              <FormControlLabel value="democrat" control={<Radio />} label="Democrat" />
-              <FormControlLabel value="republican" control={<Radio />} label="Republican" />
-              <FormControlLabel value="all" control={<Radio />} label="All" />
+              <FormControlLabel value="democrat" eventkey="democrat" control={<Radio />} label="Democrat" />
+              <FormControlLabel value="republican" eventkey="republican" control={<Radio />} label="Republican" />
+              <FormControlLabel value="all" eventkey="All" control={<Radio />} label="All" />
 
             </RadioGroup>
           </FormControl>
@@ -77,13 +92,14 @@ function App() {
           <h2>Supporters</h2>
           <h3>
 
-            {cart.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+            {
+            cart.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
             // <p>Bakery Item {index}</p> // replace with BakeryItem component
             <div>
               {item}
             </div>
             ))}
-          
+
           </h3>
           {/* <h3>Total: {total}</h3> */}
     
@@ -91,9 +107,6 @@ function App() {
         </div>
 
       </div>
-
-
-
 
       <div className="people">
         {data.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
