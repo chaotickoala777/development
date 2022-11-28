a92e96b17d23082376f32a3c6056b78a872370f0
jlum67 <90434758+jlum67@users.noreply.github.com>
2022-11-28 10:58:50 -0500

    cart all done

--- src/components/Politician.js
+++ src/components/Politician.js
@@ -42,12 +42,15 @@ export default function Politician({name, title, leg, age, party, image, cart, s
             }>In Support</button>
 
             <button onClick={() => {
-                    console.log({name})
-                    const newList = cart.filter((item) => item.name !== name);
+                    let prev = cart
+                    const newList = cart.filter((item) => item !== name);
                     setCart(newList)
-                    count !== 0
-                    ? setCount(count - 1)
-                    : setCount(0)
+                    // count !== 0
+                    // ? setCount(count - 1)
+                    // : setCount(0)
+                    ! prev.includes(name)
+                    ? setCount(count)
+                    : setCount(count - 1)
                     
                     console.log(newList)
                     console.log(cart)
